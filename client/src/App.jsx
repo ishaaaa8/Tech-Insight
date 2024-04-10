73cdb395b07ab82c4faeb9104e34c78f7a760261
ishaaaa8 <itsisha4884@gmail.com>
2024-04-10 22:00:37 +0530

    Footer Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
 import Header from './components/Header'
+import FooterC from './components/FooterC'
 export default function App() {
   return (
     <BrowserRouter>
@@ -19,6 +20,7 @@ export default function App() {
         <Route path='/sign-up' element={<SignUp/>} />
         <Route path='/projects' element={<Projects/>} />
       </Routes>
+      <FooterC/>
     </BrowserRouter>
   )
 }
