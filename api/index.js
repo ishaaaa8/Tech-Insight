53cf1d1a82abbb9d860af40f72cfb270413888a8
ishaaaa8 <itsisha4884@gmail.com>
2024-04-12 14:18:02 +0530

    update user api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 dotenv.config();
+import cookieParser from 'cookie-parser';
 
 mongoose.connect(process.env.MONGO)
 .then( () => {
@@ -11,9 +12,10 @@ mongoose.connect(process.env.MONGO)
 })
 .catch((err) => {
     console.log(err);
-});
+}); 
 const app=express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log('Server on port 3000!');
