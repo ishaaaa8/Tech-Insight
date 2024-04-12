8ebfe2fd3d6d043e7a7a8d3fac49e5a5c9a409e8
ishaaaa8 <itsisha4884@gmail.com>
2024-04-12 12:19:31 +0530

    user profile image upload and stored in firebase

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,19 +1,148 @@
-import {Button, TextInput} from 'flowbite-react';
-import {useSelector} from 'react-redux'
+import { Alert, Button, Modal, ModalBody, TextInput } from 'flowbite-react';
+import { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+// import {
+//   updateStart,
+//   updateSuccess,
+//   updateFailure,
+//   deleteUserStart,
+//   deleteUserSuccess,
+//   deleteUserFailure,
+//   signoutSuccess,
+// } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
+
 export default function DashProfile() {
-    const {currentUser} = useSelector(state => state.user);
-    if (!currentUser) {
-        return <div>Loading...</div>; // or any loading indicator
-    }
+  const { currentUser, error, loading } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+  const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+  const [updateUserError, setUpdateUserError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [formData, setFormData] = useState({});
+  const filePickerRef = useRef();
+  const dispatch = useDispatch();   console.log(imageFileUploadProgress,imageFileUploadError)
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+          setImageFile(file);
+          setImageFileUrl(URL.createObjectURL(file));
+        }
+      };
+
+      useEffect(() => {
+        if(imageFile){
+            uploadImage();
+        }
+      },[imageFile]);
+      const uploadImage = async () => {
+        // service firebase.storage {
+        //   match /b/{bucket}/o {
+        //     match /{allPaths=**} {
+        //       allow read,write;
+        //     }
+        //   }
+        // }
+        setImageFileUploading(true);
+    setImageFileUploadError(null);
+        const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    // const fileName=imageFile.name;
+    const storageRef = ref(storage, fileName);
+    
+    // Log file metadata
+    console.log("File Name:", imageFile.name);
+    console.log("File Size:", imageFile.size, "bytes");
+    console.log("Content Type:", imageFile.type);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        console.log(error);
+        setImageFileUploadError(
+          'Could not upload image (File must be less than 2MB)'
+        );
+        setImageFileUploadProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+        setImageFileUploading(false);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+          setFormData({ ...formData, profilePicture: downloadURL });
+          setImageFileUploading(false);
+        });
+      }
+    );
+  };
+      
+    // if (!currentUser) {
+    //     return <div>Loading...</div>; // or any loading indicator
+    // }
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' >
-
-        <img src={currentUser.profilePicture} alt='user' className='rounded-full w-full h-full object-cover border-8 border-[lightgray]'></img>
-
+        <input type='file' accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden/>
+        <div
+          className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'
+          onClick={() => filePickerRef.current.click()}
+        >
+          {imageFileUploadProgress && (
+            <CircularProgressbar
+              value={imageFileUploadProgress || 0}
+              text={`${imageFileUploadProgress}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: '100%',
+                  height: '100%',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(62, 152, 199, ${
+                    imageFileUploadProgress / 100
+                  })`,
+                },
+              }}
+            />
+          )}
+          <img
+            src={imageFileUrl || currentUser.profilePicture}
+            alt='user'
+            className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${
+              imageFileUploadProgress &&
+              imageFileUploadProgress < 100 &&
+              'opacity-60'
+            }`}
+          />
         </div>
+
+        {imageFileUploadError && (<Alert color='failure'>{imageFileUploadError}</Alert>)}
+
         <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username}></TextInput>
         <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email}></TextInput>
         <TextInput type='password' id='password' placeholder='password'></TextInput>
