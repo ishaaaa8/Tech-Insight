5944ad3ab7239664a684973cd187625e6aaf101a
ishaaaa8 <itsisha4884@gmail.com>
2024-04-13 18:50:55 +0530

    Posts component to dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import {useLocation} from 'react-router-dom'
 import {useState,useEffect} from "react";
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPosts';
 
 
 
@@ -20,6 +21,7 @@ export default function Dashboard() {
     <div className='min-h-screen flex flex-col md:flex-row'>
       <div className='md:w-56'><DashSidebar/></div>
       {tab==='profile' && <DashProfile/>}
+      {tab==='posts' && <DashPosts/>}
     </div>
   )
 }
