f5cb310cf6e77807e32c611c75c706c7b9ba9634
ishaaaa8 <itsisha4884@gmail.com>
2024-04-14 01:57:20 +0530

    dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import {Sidebar} from 'flowbite-react';
-import {HiUser , HiArrowSmRight, HiDocumentText, HiOutlineUserGroup} from 'react-icons/hi';
+import {HiUser , HiArrowSmRight, HiDocumentText, HiOutlineUserGroup,HiChartPie,} from 'react-icons/hi';
 
 import { useState,useEffect } from 'react';
 import {useLocation} from 'react-router-dom';
@@ -39,6 +39,17 @@ export default function DashSidebar() {
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
             <Sidebar.ItemGroup className='flex flex-col gap-1'>
+            {currentUser && currentUser.isAdmin && (
+            <Link to='/dashboard?tab=dash'>
+              <Sidebar.Item
+                active={tab === 'dash' || !tab}
+                icon={HiChartPie}
+                as='div'
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
                 <Link to='/dashboard?tab=profile'>
                 <Sidebar.Item active={tab==='profile'} icon={HiUser} label={currentUser.isAdmin? 'Admin' : 'User'} labelColor='dark' as='div'>Profile</Sidebar.Item>
                 </Link>
